1b964cc96b3e29a69624e0c4728fc652579fb073
Megan Carnaghi <carnagh5@gmail.com>
2020-11-20 13:52:39 -0500

    Created server in server.js file.

---
+++ server.js
@@ -0,0 +1,23 @@
+//Dependencies
+const express = require("express");
+
+// Express Configuration
+// Tells node that we are creating an "Express" server
+const app = express();
+
+// Sets an initial port for our server
+const PORT = process.env.PORT || 3000
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded ({extended: true}));
+app.use(express.json());
+
+// // Router
+// // This points our server to a series of "route" files.
+// // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// require("./routes/apiroutes")(app);
+// require("./routes/htmlroutes")(app);
+
+// Listener
+// This code "starts" our server.
+app.listen(PORT, () => console.log(`Server listening on PORT: ${PORT}`));
